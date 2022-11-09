155c977e69e5744cdc88e2f6a8368197994d6d1c
YR749 <ypscr21.3182@gmail.com>
2022-11-09 16:59:56 +0900

    [add] ステージ解放の処理を追加

---
+++ Assets/menuScene/KeepData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data")]
+public class KeepData : ScriptableObject
+{
+    public int _countClear;
+}
