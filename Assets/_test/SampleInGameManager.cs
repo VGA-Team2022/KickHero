432696ed1ab6155b0e975884ab010f5e733220fb
HarashimaYoshikazu <harashima318@gmail.com>
2022-11-11 17:37:31 +0900

    [fix]サンプルのスクリプトとシーンのフォルダの配置を変更。

--- Assets/_test/SampleInGameManager.cs
+++ Assets/_test/SampleInGameManager.cs
@@ -2,26 +2,28 @@ using UnityEngine;
 using UnityEngine.UI;
 using Cysharp.Threading.Tasks;
 
-public class InGameManager : AbstructScene
+namespace alpha
+{
+    public class SampleInGameManager : AbstructScene
 {
     /// <summary>シーン内で使用する文字列</summary>
     protected string _message = "";
 
     [Header("コンポーネント")]
-    [SerializeField,Tooltip("押下時にロードが呼ばれるボタン")]
+    [SerializeField, Tooltip("押下時にロードが呼ばれるボタン")]
     Button _loadButton;
 
-    [SerializeField,Tooltip("押下時に変数が変更されるボタン")]
+    [SerializeField, Tooltip("押下時に変数が変更されるボタン")]
     Button _inputButton;
 
-    [SerializeField,Tooltip("次のシーンに渡したい文字列を入力するフィールド")]
+    [SerializeField, Tooltip("次のシーンに渡したい文字列を入力するフィールド")]
     protected InputField _inputField = null;
 
-    [SerializeField,Tooltip("現在保持されている変数を表示するText")]
+    [SerializeField, Tooltip("現在保持されている変数を表示するText")]
     protected Text _text;
 
     [Header("ロード関連")]
-    [SerializeField,Tooltip("ロード先のシーン名")]
+    [SerializeField, Tooltip("ロード先のシーン名")]
     string _sceneName;
 
     protected override void OnAwake()
@@ -38,7 +40,7 @@ public class InGameManager : AbstructScene
             else
             {
                 _sceneOperator.SetUp(_inputField.text);
-            }         
+            }
         });
     }
     public override async UniTask Load(string message)
@@ -60,3 +62,5 @@ public class InGameManager : AbstructScene
         await UniTask.Yield();
     }
 }
+}
+
