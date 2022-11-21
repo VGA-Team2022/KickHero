3e047ae5b955534c7076d7f059ad481caae8a6da
SubNiitsuma <sakezuma@gmail.com>
2022-11-22 00:39:25 +0900

    [update]fadeを外から呼べるようにした

--- Assets/Scripts/InGame/Battle/Niitsuma/TransitionFade.cs
+++ Assets/Scripts/InGame/Battle/Niitsuma/TransitionFade.cs
@@ -22,17 +22,17 @@ public class TransitionFade : UnityEngine.UI.Graphic
 
     void UpdateCutOutMask(float range)
     {
-        if (range <= 0f)
-        {
-            if (enabled)
-            {
-                material.SetFloat("_Range", 1 - range);
-                enabled = false;
-            }
-            return;
-        }
+        //if (range <= 0f)
+        //{
+        //    if (enabled)
+        //    {
+        //        material.SetFloat("_Range", 1 - range);
+        //        enabled = false;
+        //    }
+        //    return;
+        //}
 
-        if (!enabled) { enabled = true; }
+        //if (!enabled) { enabled = true; }
 
         material.SetFloat("_Range", 1 - range);
     }
@@ -41,6 +41,54 @@ public class TransitionFade : UnityEngine.UI.Graphic
     {
         material.SetTexture("_MaskTex", texture);
         material.SetColor("_Color", color);
+    }
+
+    IEnumerator StartFadeIn(float time, Action action)
+    {
+        Range = 1;
+        float _time = 0;
+        while (true)
+        {
+            yield return null;
+            _time += Time.deltaTime;
+            Range = 1.0f - _time / time;
+            if (Range <= 0) { break; }
+        }
+        if (action != null) { action(); }
+    }
+    IEnumerator StartFadeOut(float time, Action action)
+    {
+        Range = 0;
+        float _time = 0;
+        while (true)
+        {
+            yield return null;
+            _time += Time.deltaTime;
+            Range = _time / time;
+            if (Range >= 1) { break; }
+        }
+        if (action != null) { action(); }
+    }
+    public void FadeOut(float time, Action action)
+    {
+        StopAllCoroutines();
+        StartCoroutine(StartFadeOut(time, action));
+    }
+
+    public void FadeOut(float time)
+    {
+        FadeOut(time, null);
+    }
+
+    public void FadeIn(float time, Action action)
+    {
+        StopAllCoroutines();
+        StartCoroutine(StartFadeIn(time, action));
+    }
+
+    public void FadeIn(float time)
+    {
+        FadeIn(time, null);
     }
 
 #if UNITY_EDITOR
