48bcedf9777bac44d102fbf1be463ef47a953f77
SubNiitsuma <sakezuma@gmail.com>
2022-12-09 02:33:50 +0900

    [update]エフェクトマネージャー完成。インスペクター上で設定したエフェクトたちは関数でvector3と要素数を指定すれば生成できるようにした。

--- Assets/_test/Camera_move/EffectManager.cs
+++ Assets/_test/Camera_move/EffectManager.cs
@@ -2,31 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class PoolEffect
+{
+    [Tooltip("エフェクトのプレハブ")]
+    public EffectSetting Prefab;
+    [Tooltip("生成するエフェクトの親となるTransform(オブジェクト)")]
+    public Transform Parent;
+    [Tooltip("最大生成数")]
+    public int CapacitySize = 10;
+
+    public ObjectPool<EffectSetting> EffPool = new ObjectPool<EffectSetting>();
+}
+
 public class EffectManager : MonoBehaviour
 {
-    [SerializeField, Tooltip("エフェクトのプレハブ")]
-    EffectSetting[] _effPrefab;
-    [SerializeField,Tooltip("生成するエフェクトの親となるオブジェクト")]
-    Transform Parent;
-    [SerializeField, Tooltip("最大スポーン数")]
-    int _capacitySize = 10;
+    [SerializeField, Tooltip("生成するエフェクト")]
+    PoolEffect[] Effects;
 
-    ObjectPool<EffectSetting> _effPool = new ObjectPool<EffectSetting>();
+    
     // Start is called before the first frame update
     void Start()
     {
-        _effPool.SetBaseObj(_effPrefab[0],Parent);
-        _effPool.SetCapacity(_capacitySize);
+        foreach (var eff in Effects)
+        {
+            eff.EffPool.SetBaseObj(eff.Prefab, eff.Parent);
+            eff.EffPool.SetCapacity(eff.CapacitySize);
+        }
     }
+
     /// <summary>
-    /// エフェクトを特定の場所に生成する
+    /// 指定したIDのエフェクトを特定の場所に生成する
     /// </summary>
-    /// <param name="Pos"></param>
-    public void InstancetiateEff(Vector3 Pos)
+    /// <param name="Pos">生成時のPostion</param>
+    /// <param name="ID">インスペクター上で設定した配列の要素数</param>
+    public void InstancetiateEff(Vector3 Pos ,int ID)
     {
-        var eff = _effPool.Instancetiate();
+        if(ID < 0 || ID >= Effects.Length ) { return; }
 
-        eff.gameObject.transform.position = Pos;    
+        var eff = Effects[ID].EffPool.Instancetiate();
+
+        eff.gameObject.transform.position = Pos;
     }
-
 }
