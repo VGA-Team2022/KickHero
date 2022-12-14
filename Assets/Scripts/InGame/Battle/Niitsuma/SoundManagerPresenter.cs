570f67173146684bad2652a1eb3365f0414a2d62
SubNiitsuma <sakezuma@gmail.com>
2022-12-15 01:49:02 +0900

    [update]soundManagerをMVPパターンにした。

---
+++ Assets/Scripts/InGame/Battle/Niitsuma/SoundManagerPresenter.cs
@@ -0,0 +1,76 @@
+using CriWare;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class SoundManagerPresenter : MonoBehaviour
+{
+    [Header("View")]
+    [SerializeField] SoundManagerView _view;
+
+    [Header("AtomSorce")]
+    [SerializeField] CriAtomSource _atomSESource;
+    [SerializeField] CriAtomSource _atomBGMSource;
+    [SerializeField] CriAtomSource _atomVoiceSorce;
+
+    [SerializeField] float _changeSpeed = 0.5f;
+
+    ReactiveProperty<InGameCycle.EventEnum> _eventProperty;
+
+    SoundManagerModel _model = new SoundManagerModel();
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    public void Init(System.Action<InGameCycle.EventEnum> eventAction)
+    {
+        //シーケンスの遷移を指定。
+        _eventProperty = new ReactiveProperty<InGameCycle.EventEnum>(InGameCycle.EventEnum.None);
+        _eventProperty.Subscribe(eventAction).AddTo(this.gameObject);
+        ValueSet();
+    }
+
+    public void Init()
+    {
+        ValueSet();
+    }
+
+    public void CriAtomSEPlay(string cueName)
+    {
+        _model.CriAtomPlay(CueSheet.SE, cueName);
+    }
+    public void CriAtomMEPlay(string cueName)
+    {
+        _model.CriAtomPlay(CueSheet.ME, cueName);
+    }
+    public void CriAtomBGMPlay(string cueName)
+    {
+        _model.CriAtomBGMPlay(cueName);
+    }
+    public void CriAtomVoicePlay(string cueName)
+    {
+        _model.CriAtomVoicePlay(cueName);
+    }
+    public void CriAtomStop(CriAtomSource cri)
+    {
+        _model.CriAtomStop(cri);
+    }
+
+    public void SetVolume()
+    {
+        _model.SetVolume(_model.AtomBGMSource, _view.BGMSlider.value);
+        _model.SetVolume(_model.AtomSESource, _view.SESlider.value);
+        _model.SetVolume(_model.AtomVoiceSorce, _view.VoiceSlider.value);
+    }
+    void ValueSet()
+    {
+        _model.AtomBGMSource = _atomBGMSource;
+        _model.AtomSESource = _atomSESource;
+        _model.AtomVoiceSorce = _atomVoiceSorce;
+        _model.ChangeSpeed = _changeSpeed;
+        SetVolume();
+    }
+}
